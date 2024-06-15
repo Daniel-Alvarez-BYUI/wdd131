b388a4889ac64b68be0cdfe54f669b13babc17ab
Daniel Alvarez <104532965+Daniel-Alvarez-BYUI@users.noreply.github.com>
2024-06-15 19:49:06 -0400

    Add files via upload

---
+++ wdd131/scripts/gatedates.js
@@ -0,0 +1,5 @@
+// scripts.js
+document.addEventListener("DOMContentLoaded", function() {
+    var lastModified = document.lastModified;
+    document.getElementById("lastModified").textContent = lastModified;
+});
